748389a0cde16f6b6eb66b5b438f1c973ce1592b
Kyla Allen <themightyflorp@Kylas-MacBook-Air.local>
2018-11-07 16:38:40 -0600

    did the while exercise

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,21 @@
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+
+        //While
+//        int i = 5;
+//        while(i <= 15){
+//            System.out.print(i++);
+//            System.out.print(" ");
+//        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
