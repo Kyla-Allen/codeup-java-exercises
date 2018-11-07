4ee68286b8393793a4dcabdfddf864f0953d7d15
Kyla Allen <themightyflorp@Kylas-MacBook-Air.local>
2018-11-07 10:42:48 -0600

    syntax exercises were added

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,53 @@
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.println("Howdy");
+
+//        System.out.println("Hello World");
+//
+//        int myFavoriteNumber = 7;
+//        System.out.println(myFavoriteNumber);
+//
+//        String myString = "Howdy Howdy Howdy";
+//        System.out.println(myString);
+//
+//        float myNumber = 3.14F;
+//        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++); //5- it displays then is incremented
+//        System.out.println(x); //6
+
+//        int x = 5;
+//        System.out.println(++x); //6- increments then displays
+//        System.out.println(x); //6
+
+
+//        int class = 6; //Can't use a keyword to identify a variable
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//        System.out.println(three); //Runtime error
+
+//        int three = (int) "three"; //Compile error
+
+//        int x =4;
+//        x += 5; //9
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x; //12
+//        System.out.println(y);
+
+
+//        int x = 10;
+//        int y = 2;
+//        x /= y; //5
+//        y -= x; //-3
+//        System.out.println(x);
+//        System.out.println(y);
+
+
+
+
     }
 }
