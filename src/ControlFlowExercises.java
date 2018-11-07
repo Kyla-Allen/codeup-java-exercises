ed750cab171c5de82d1cdd6232306c3057f385de
Kyla Allen <themightyflorp@Kylas-MacBook-Air.local>
2018-11-07 16:53:38 -0600

    did the do-while exercises

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -8,6 +8,21 @@ public class ControlFlowExercises {
 //            System.out.print(" ");
 //        }
 
+        //Do While
+//        int start = 100;
+//        do {
+//            System.out.println(start);
+//            start -= 5;
+//        } while (start >= -10);
+//
+
+//        int squareThis = 2;
+//        do {
+//            System.out.println(squareThis);
+//            squareThis = squareThis * squareThis;
+//
+//        } while (squareThis <= 1000000); //Okay, so this will go on forever because once it gets to 65535, squaring it will put it past the max for int. So it resets back to zero.
+
 
 
 
