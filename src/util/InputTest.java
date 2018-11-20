3f5e5231b1f9f24786964fc80b8d060ba0a52575
Kyla Allen <themightyflorp@Kylas-MacBook-Air.local>
2018-11-20 14:03:42 -0600

    did the exception exercise

--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -9,10 +9,10 @@ public class InputTest {
         Input input = new Input(sc);
 
 //        System.out.println(input.getDouble("Enter a double"));
-//        input.getDouble(1, 12, "Enter a number between 1 and 12");
+        input.getDouble(1, 12, "Enter a number between 1 and 12");
 
 //        System.out.println(input.getInt("Enter a whole number"));
-//        input.getDouble(4, 9, "Enter a number between 4 and 9");
+//        input.getInt(4, 9, "Enter a number between 4 and 9");
 
 //        System.out.println(input.getString("Enter a String"));
 //        System.out.println("You entered yes: " + input.yesNo("Enter yes or no"));
