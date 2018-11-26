943298985b2619ce4801ab1e3cfc0e3752117df1
Kyla Allen <themightyflorp@Kylas-MacBook-Air.local>
2018-11-26 13:51:20 -0600

    added an overide file capability

--- src/FileIOExercise.java
+++ src/FileIOExercise.java
@@ -1,6 +1,8 @@
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.List;
 
 public class FileIOExercise {
@@ -18,10 +20,23 @@ public class FileIOExercise {
         return output;
     }
 
+//    WRITING TO FILE
+    public static void write2file (Path path, List<String> addThis){
+        try{
+            Files.write(path, addThis);
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
 
 //    MAIN
     public static void main (String[] args){
         String path = "src/test.txt";
+        Path p = Paths.get(path);
+        List<String> lines = Arrays.asList("Eric", "Charles", "KQEWHad");
+
+        System.out.println(getFileData(path));
+        write2file(p, lines);
         System.out.println(getFileData(path));
     }
 }
