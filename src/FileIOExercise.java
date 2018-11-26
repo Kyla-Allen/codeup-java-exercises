060d9a993ab231832175fa4145119a7385ed2ac9
Kyla Allen <themightyflorp@Kylas-MacBook-Air.local>
2018-11-26 13:56:24 -0600

    added the capability to add to a file

--- src/FileIOExercise.java
+++ src/FileIOExercise.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.Arrays;
 import java.util.List;
 
@@ -29,14 +30,25 @@ public class FileIOExercise {
         }
     }
 
+//    ADDING TO FILE
+    public static void add2file (Path path, List<String> addThis){
+        try{
+            Files.write(path, addThis, StandardOpenOption.APPEND);
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
 //    MAIN
     public static void main (String[] args){
         String path = "src/test.txt";
         Path p = Paths.get(path);
         List<String> lines = Arrays.asList("Eric", "Charles", "KQEWHad");
 
-        System.out.println(getFileData(path));
         write2file(p, lines);
+        System.out.println(getFileData(path));
+        lines = Arrays.asList("Cherry", "Espanol");
+        add2file(p,lines);
         System.out.println(getFileData(path));
     }
 }
